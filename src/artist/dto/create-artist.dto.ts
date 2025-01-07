46bfbee504ea8427849e7d8475f14b4e5e9988bc
Щетинин Артур <00banka@gmail.com>
2025-01-07 22:45:11 +0500

    #1 - Сделал dto для create-artist

---
+++ src/artist/dto/create-artist.dto.ts
@@ -0,0 +1,23 @@
+import { Expose } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateArtistDto {
+  @Expose()
+  @IsNotEmpty({ message: 'Укажите имя исполнителя' })
+  @IsString({ message: 'Имя исполнителя должно быть строкой' })
+  name!: string;
+
+  @Expose()
+  @IsOptional()
+  photo?: Express.Multer.File;
+
+  @Expose()
+  @IsNotEmpty({ message: 'Укажите информацию о исполнителе' })
+  @IsString({ message: 'Информация о исполнителе должна быть строкой' })
+  information!: number;
+
+  // @Expose()
+  // @IsNotEmpty({ message: 'Укажите категорию продукта' })
+  // @IsNumberString({}, { message: 'Укажите корректную категорию' })
+  // categoryId: string;
+}
