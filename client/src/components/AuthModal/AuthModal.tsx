dde6c4fa492a0f24e5455bf86dcebcd6b00442f3
Щетинин Артур <00banka@gmail.com>
2025-01-21 21:47:38 +0500

    #11 - Добавил общий компонент для модального окна авторизации

---
+++ client/src/components/AuthModal/AuthModal.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useState } from 'react';
+import { Flex, Modal } from 'antd';
+import { useForm } from 'antd/es/form/Form';
+
+interface Props {
+  type: 'register' | 'login';
+  isOpen?: boolean;
+}
+
+export function AuthModal({ type, isOpen }: Props) {
+  const [isModalOpen, setIsModalOpen] = useState(isOpen || false);
+  const [form] = useForm();
+
+  const onOk = () => {
+    form.submit();
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const onCancel = () => {
+    form.resetFields();
+    setIsModalOpen(false);
+  };
+
+  const buttonText = type === 'register' ? 'Register' : 'Login';
+  const title = type === 'register' ? 'Registration' : 'Authorization';
+
+  return (
+    <>
+      <a onClick={showModal}>{buttonText}</a>
+      <Modal
+        title={title}
+        open={isModalOpen}
+        okText={buttonText}
+        onOk={onOk}
+        onCancel={onCancel}
+      >
+        <Flex gap="middle" align="start" justify="center"></Flex>
+      </Modal>
+    </>
+  );
+}
