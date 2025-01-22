3529201f2e6af765a69be1c673666d5388322fd8
Щетинин Артур <00banka@gmail.com>
2025-01-22 16:13:45 +0500

    #12 - Доработал hoc

--- client/src/hoc/withAuth.tsx
+++ client/src/hoc/withAuth.tsx
@@ -1,19 +1,49 @@
-"use client";
+'use client';
 
-import { useAppSelector } from "@/store/hooks";
-import { redirect } from "next/navigation";
-import { ElementType, useEffect } from "react";
+import { redirect } from 'next/navigation';
+import { ElementType, useEffect } from 'react';
+import { message } from 'antd';
+import { useAppSelector } from '@/store/hooks';
 
-export const withAuth = (WrappedComponent: ElementType) => {
+export const withAuth = (
+  WrappedComponent: ElementType,
+  resultMessage: boolean = false,
+) => {
   return function WithAuth(props: object) {
+    const [messageApi, contextHolder] = message.useMessage();
     const { user } = useAppSelector((state) => state.user);
 
+    const displaySuccess = () => {
+      messageApi.open({
+        type: 'success',
+        content: 'Success!',
+      });
+    };
+
+    const displayError = () => {
+      messageApi.open({
+        type: 'error',
+        content: 'You are not authorized',
+      });
+    };
+
     useEffect(() => {
-      if (!user) {
-        redirect("/login");
+      if (!user && !resultMessage) {
+        redirect('/login');
+      } else if (!user && resultMessage) {
+        displayError();
+      } else if (user && resultMessage) {
+        displaySuccess();
       }
     });
 
-    return <WrappedComponent {...props} />;
+    if (!user && !resultMessage) return null;
+
+    return (
+      <>
+        {contextHolder}
+        <WrappedComponent {...props} />
+      </>
+    );
   };
 };
