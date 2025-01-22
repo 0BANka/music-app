db7870a73d99fa49ef821847dcae1f8363a32dfe
Щетинин Артур <00banka@gmail.com>
2025-01-22 19:19:56 +0500

    #13 - Сделал компонент TracksHistoryList

--- client/src/components/TracksHistoryList/TracksHistoryList.tsx
+++ client/src/components/TracksHistoryList/TracksHistoryList.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
+import { ITrackHistory } from '@/interfaces/ITrackHistory';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { fetchTracksHistory } from '@/features/tracksHistorySlice';
+import { TrackHistoryItem } from '../TrackHistoryItem/TrackHistoryItem';
+import { Loader } from '../Loader/Loader';
+
+import './TracksHistoryList.sass';
 
 export function TracksHistoryList() {
   const dispatch = useAppDispatch();
-  const { tracks, loading } = useAppSelector((state) => state.tracks);
+  const { tracks, loading } = useAppSelector((state) => state.tracksHistory);
   const { user } = useAppSelector((state) => state.user);
-  const [data, setData] = useState<ITrack[]>([]);
+  const [data, setData] = useState<ITrackHistory[]>([]);
+
+  useEffect(() => {
+    if (user?.token) {
+      dispatch(fetchTracksHistory(user.token));
+    }
+  }, [dispatch, user?.token]);
+
+  useEffect(() => {
+    if (tracks.length > 0 && Array.isArray(tracks)) {
+      setData(tracks);
+    } else {
+      setData([]);
+    }
+  }, [tracks]);
 
   return (
     <div className="container">
@@ -12,7 +33,12 @@ export function TracksHistoryList() {
         <h1 className="tracks-history-list-title">Tracks History</h1>
         {loading && <Loader />}
         {data.length > 0 &&
-          data.map((element) => <TrackItem key={element.id} track={element} />)}
+          data.map((element) => (
+            <TrackHistoryItem
+              key={element.datetime}
+              trackHistoryItem={element}
+            />
+          ))}
       </div>
     </div>
   );
