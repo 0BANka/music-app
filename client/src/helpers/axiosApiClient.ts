5ea0054cbc0a7afa9f1b3fa2843e7e429b1078bf
Щетинин Артур <00banka@gmail.com>
2025-02-03 12:47:01 +0500

    #21 - Настроил store и роли на клиентской части

--- client/src/helpers/axiosApiClient.ts
+++ client/src/helpers/axiosApiClient.ts
@@ -1,5 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
+import { Store } from '@reduxjs/toolkit';
+import { RootState } from '@/store';
 
 export const axiosApiClient = axios.create({
   baseURL: process.env.SERVER_URL,
+});
+
+type AppStore = Store<RootState>;
+let store: AppStore;
+
+export function injectStore(_store: AppStore) {
+  store = _store;
+}
+
+axiosApiClient.interceptors.request.use((config) => {
+  try {
+    if (store) {
+      config.headers['Authorization'] = store.getState().user.user?.token;
+    }
+  } catch {}
+
+  return config;
 });
