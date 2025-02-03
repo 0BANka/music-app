dbe56ad31fcc90950eb04d813f3f808955ec90bb
Щетинин Артур <00banka@gmail.com>
2025-02-03 11:52:57 +0500

    #21 - Доработал фикстуры и роуты artist

--- server/src/artist/artist.service.ts
+++ server/src/artist/artist.service.ts
@@ -1,24 +1,61 @@
 import { Injectable } from '@nestjs/common';
-import { CreateArtistDto } from './dto/create-artist.dto';
-import { Artist } from './entities/artist.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { Artist } from './entities/artist.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class ArtistService {
   constructor(
     @InjectRepository(Artist)
     private artistRepository: Repository<Artist>,
+
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
-  async create(createArtistDto: CreateArtistDto, photo?: Express.Multer.File) {
+  async create(
+    createArtistDto: CreateArtistDto,
+    token: string,
+    photo?: Express.Multer.File,
+  ) {
+    const user = await this.usersRepository.findOne({
+      where: {
+        token,
+      },
+    });
+
     return await this.artistRepository.save({
       ...createArtistDto,
       photo: photo?.filename || 'no-photo-available.png',
+      user: String(user.id),
     });
   }
 
-  async findAll(): Promise<Artist[]> {
-    return this.artistRepository.find();
+  async findAll(token: string) {
+    const currentUser = token
+      ? await this.usersRepository.findOne({
+          where: { token },
+        })
+      : null;
+
+    const artists = await this.artistRepository.find();
+
+    const artistsProcessed = artists.map((artist) => {
+      return {
+        id: artist.id,
+        name: artist.name,
+        info: artist.info,
+        photo: artist.photo,
+        user: artist.user,
+        isPublish: artist.isPublish,
+        isMyPost: currentUser
+          ? String(artist.user) === String(currentUser.id)
+          : false,
+      };
+    });
+
+    return artistsProcessed;
   }
 }
--- server/src/db/factories/artist.factory.ts
+++ server/src/db/factories/artist.factory.ts
@@ -7,6 +7,8 @@ export const artistFactory = setSeederFactory(Artist, async () => {
   artist.name = faker.music.artist();
   artist.info = faker.person.bio();
   artist.photo = faker.image.urlPicsumPhotos();
+  artist.isPublish = true;
+  artist.user = String(faker.number.int({ min: 1, max: 2 }));
 
   return artist;
 });
